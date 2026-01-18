d678f2e581d07a96e2a24c04ce6d7807fe1583d8
Эльмира Кадырмаева <elmira1kadyrmaeva2@gmail.com>
2026-01-18 14:44:50 +0500

    Исправлен выход из системы

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),  # login/logout/password_*
     path("", include("planner.urls")),
+
+    # ВАЖНО: переопределяем logout, чтобы работал по GET
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
